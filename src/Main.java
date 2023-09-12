766ea018689d26c6255e821c24aa2909f2e2d1c4
Admin <phamphudong0112@gmail.com>
2023-09-12 07:51:47 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+import controller.CountProgramming; // sử dụng lệnh trong ....
+
+public class Main {
+    public static void main(String[] args){
+        new CountProgramming().run(); // khai báo đối tượng trong lớp đó r chạy
+    }
+}
